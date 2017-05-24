b496945ff9a0812cdef6f1916f9b60f7c9942425
Sven Erik Vinkemeier <svenvinkemeier@live.de>
2017-05-24 11:37:21 +0200

    Hackathon preparation

---
+++ HannoverVRHackathonPlatformer.Game/Assets/VR/VRCamera.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using UnityEngine;
+
+public class VRCamera : MonoBehaviour
+{
+    private Vector3 _mousePosition;
+
+    public Shader DistortionShader;
+    public VRDeviceSettings VRDeviceSettings;
+
+    private void Start()
+    {
+        SetupCameras();
+        gameObject.AddComponent<VRHeadTracking>();
+    }
+
+    private void SetupCameras()
+    {
+#if UNITY_WSA
+        GetComponent<Camera>().enabled = false;
+
+        var left = new GameObject("Left Eye").transform;
+        var right = new GameObject("Right Eye").transform;
+
+        left.SetParent(transform);
+        right.SetParent(transform);
+
+        left.localPosition = new Vector3(VRDeviceSettings.Ipd / -200f, 0, 0);
+        right.localPosition = new Vector3(VRDeviceSettings.Ipd / 200f, 0, 0);
+
+        var leftCam = left.gameObject.AddComponent<Camera>();
+        leftCam.rect = new Rect(0, 0, .5f, 1);
+        leftCam.fieldOfView = 95;
+
+        var rightCam = right.gameObject.AddComponent<Camera>();
+        rightCam.rect = new Rect(.5f, 0, .5f, 1);
+        rightCam.fieldOfView = 95;
+
+        left.gameObject.AddComponent<VREye>().Configure(VRDeviceSettings, DistortionShader);
+        right.gameObject.AddComponent<VREye>().Configure(VRDeviceSettings, DistortionShader);
+#endif
+    }
+}
+
+public class VREye : MonoBehaviour
+{
+    private Material _material;
+    private VRDeviceSettings _settings;
+
+    public void Configure(VRDeviceSettings settings, Shader distortionShader)
+    {
+        _settings = settings;
+        _material = new Material(distortionShader) { hideFlags = HideFlags.DontSave };
+    }
+
+    private void OnRenderImage(RenderTexture source, RenderTexture destination)
+    {
+        float xOffset = _settings.Ild - 60;
+
+        _material.SetFloat("_XOffset", xOffset);
+        _material.SetFloat("_K1", _settings.K1 * _settings.DistortionScale);
+        _material.SetFloat("_K2", _settings.K2 * _settings.DistortionScale);
+        _material.SetFloat("_K3", _settings.K3 * _settings.DistortionScale);
+        _material.SetFloat("_K4", _settings.K4 * _settings.DistortionScale);
+
+        Graphics.Blit(source, destination, _material);
+    }
+
+}
+
+[Serializable]
+public class VRDeviceSettings
+{
+    public float K1 = .39f;
+    public float K2 = .15f;
+    public float K3 = .1f;
+    public float K4 = .03f;
+    public float DistortionScale = 1;
+    public float Ipd = 60;
+    public float Ttl = 0;
+    public float Ild = 60;
+}
---
+++ HannoverVRHackathonPlatformer.Game/Assets/VR/VRHeadTracking.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+
+public class VRHeadTracking : MonoBehaviour
+{
+    private Vector3 lastMouse;
+
+    private void Start()
+    {
+        Input.gyro.enabled = true;
+        Input.gyro.updateInterval = 1.0f / 100.0f;
+    }
+
+    private void Update()
+    {
+        if (Application.isEditor)
+        {
+            if (Input.GetMouseButtonDown(1))
+                lastMouse = Input.mousePosition;
+
+            if (!Input.GetMouseButton(1))
+                return;
+
+            var deltaMouse = Input.mousePosition - lastMouse;
+            lastMouse = Input.mousePosition;
+
+            transform.rotation = transform.rotation
+                * Quaternion.AngleAxis(-deltaMouse.y * .2f, Vector3.right)
+                * Quaternion.AngleAxis(deltaMouse.x * .2f, Vector3.up);
+
+            transform.eulerAngles = new Vector3(transform.eulerAngles.x, transform.eulerAngles.y, 0);
+        }
+        else
+        {
+#if UNITY_WSA
+            var screenRotation = new Vector3(-90, 0, 90);
+
+            var rotationRate = Input.gyro.rotationRate * Time.deltaTime;
+            transform.rotation *= Quaternion.Euler(-rotationRate.x, -rotationRate.y, rotationRate.z);
+
+            var acceleration = Input.acceleration;
+
+            //perform orientation transform for landscape left
+            acceleration = new Vector3(-acceleration.x, acceleration.y, acceleration.z);
+
+            var pitchVector = Vector3.ProjectOnPlane(acceleration, Vector3.right).normalized;
+            var rollVector = Vector3.ProjectOnPlane(acceleration, Vector3.forward).normalized;
+
+            var pitch = Mathf.Atan2(-pitchVector.y, pitchVector.z) * Mathf.Rad2Deg;
+            var roll = Mathf.Atan2(rollVector.y, rollVector.x) * Mathf.Rad2Deg;
+
+            var rotation = Quaternion.Euler(new Vector3(pitch, transform.eulerAngles.y, roll) + screenRotation);
+
+            //This represents a basic complimentary filter, it's not good long term we should use a Kalman filter here [ld]
+            transform.rotation = Quaternion.Slerp(transform.rotation, rotation, 5 * Time.deltaTime);
+#endif
+        }
+    }
+}
